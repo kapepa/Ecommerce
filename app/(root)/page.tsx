8df5ffacc9b2eea8da7e593c49c8de9094d59972
kapepa <kapepayx@gmail.com>
2024-03-31 20:37:29 +0300

    prepare

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { NextPage } from "next";
+
+const HomePage: NextPage = () => {
+  return (
+    <div className="p-4">
+      <Button size="sm">Click Me</Button>
+    </div>
+  )
+}
+
+export default HomePage;
