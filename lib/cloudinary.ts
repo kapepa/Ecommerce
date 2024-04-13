ad756bb653789dd228a6449f39c77526ad16dcd6
kapepa <kapepayx@gmail.com>
2024-04-13 15:18:33 +0300

    product delete all image

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,5 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import { getImageId } from "./utils";
+import { Image } from "@prisma/client";
  
 export default cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
@@ -18,4 +19,15 @@ const cloudinaryDelete = async (url: string, have?: boolean) => {
   }
 }
 
-export { cloudinaryDelete }
+const cloudinaryManyDelete = async (images: Image[])  => {
+  try {
+    const extractUrl = images.map((img) => getImageId(img.url));
+    const results = await cloudinary.api.delete_resources(extractUrl);
+    
+    return results;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export { cloudinaryDelete, cloudinaryManyDelete }
