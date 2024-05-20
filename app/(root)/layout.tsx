f271596d4327a71ce24207be7a860451945bd798
kapepa <kapepayx@gmail.com>
2024-05-20 12:15:03 +0300

    fix store

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -14,7 +14,7 @@ const RootLayout: FC<RootLayoutProps> = async ({ children }) => {
 
   // const store = await prisma.store.findFirst({ where: { userId } });
   // Temporary, need to fix
-  const store = await prisma.store.findFirst({ where: { id: "d3db22fa-5154-49a2-b5fb-eae456fbe914" } });
+  const store = await prisma.store.findFirst({ where: { id: "e7fed646-0a7e-4220-bb4c-e7e69ceb5c78" } });
 
   if (store) redirect(`/${store.id}`);
   
