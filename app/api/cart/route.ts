f48bc763744f28658d7adb56a873168973513777
kapepa <kapepayx@gmail.com>
2024-06-17 18:00:07 +0300

    get goods by id

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -4,7 +4,7 @@ export async function GET (req: Request) {
   try {
     const { searchParams } = new URL(req.url);
     const ids = searchParams.get("ids");
-    console.log(ids)
+    console.log(ids);
 
     return NextResponse.json([1,2,3], { status: 200 });
   } catch {
