0f9901d5f77c53bfa9ff6ae7f42fbbb48cd4ffee
kapepa <kapepayx@gmail.com>
2024-06-17 17:52:53 +0300

    get goods by id

---
+++ app/api/cart/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export async function GET (req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const ids = searchParams.get("ids");
+    console.log(ids)
+
+    return NextResponse.json([1,2,3], { status: 200 });
+  } catch {
+    return NextResponse.json("Интервальная ошибка", { status: 500 })
+  }
+}
